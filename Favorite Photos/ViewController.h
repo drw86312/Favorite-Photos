c7db4207527d6a76be8c35c1767f66e3c7810fa1
drw86312 <david.warner4@gmail.com>
2014-06-02 15:05:55 -0500

    Initial Commit

---
+++ Favorite Photos/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Favorite Photos
+//
+//  Created by David Warner on 6/2/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
